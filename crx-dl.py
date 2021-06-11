99b7a5076cdf0b24b23339e4f49d02501c50654c
Sergey Zolotarev <sryze@protonmail.com>
2021-06-12 01:01:30 +0600

    Add support for Python 3

--- crx-dl.py
+++ crx-dl.py
@@ -3,9 +3,15 @@
 import argparse
 import os.path
 import sys
-import urllib
-import urllib2
-import urlparse
+
+try:
+    from urlparse import urlparse
+    from urllib import urlencode
+    from urllib2 import urlopen
+except ModuleNotFoundError:
+    from urllib.parse import urlparse
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
 
 arg_parser = argparse.ArgumentParser(description='Chrome extension downloader')
 arg_parser.add_argument('url',
@@ -15,11 +21,11 @@ arg_parser.add_argument('filename',
 options = arg_parser.parse_args(sys.argv[1:])
 
 ext_url_str = options.url
-ext_url = urlparse.urlparse(ext_url_str)
+ext_url = urlparse(ext_url_str)
 ext_id = os.path.basename(ext_url.path)
 
 crx_base_url = 'https://clients2.google.com/service/update2/crx'
-crx_params = urllib.urlencode({
+crx_params = urlencode({
     'response': 'redirect',
     'prodversion': '91.0',
     'acceptformat': 'crx2,crx3',
@@ -30,7 +36,7 @@ crx_file = options.filename
 
 print('Downloading {} to {} ...'.format(crx_url, crx_file))
 
-with open(crx_file, 'w') as file:
-    file.write(urllib2.urlopen(crx_url).read())
+with open(crx_file, 'wb') as file:
+    file.write(urlopen(crx_url).read())
 
 print('Success!')
